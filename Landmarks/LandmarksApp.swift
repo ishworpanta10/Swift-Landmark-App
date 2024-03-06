4062b0696d6fe2405547438313d02e1d16c6a3f5
ishworpanta100 <ishwor.panta@outcodesoftware.com>
2024-03-06 14:27:44 +0545

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ishwor on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
